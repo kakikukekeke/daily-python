be7ea59a9dbb257bfb60814d6a7e73bead3861e5
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2025-12-30 21:42:59 +0900

    Create nation.py

---
+++ 2025-12-30-Day11/nation.py
@@ -0,0 +1,11 @@
+def number(x):
+  return x % 3 == 0
+  
+nums = [1,2,3,4,5,6,7,8,9]
+result = []
+def zero_multiple_of_three(nums):
+  for s in nums:
+    if number(s):
+      result.append(s)
+    elif number
+
