6d10f71d0036edad410f3fefc3851cadc5fa028b
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-01-01 18:28:57 +0900

    Create main.py

---
+++ 2026-1-1-Day13/main.py
@@ -0,0 +1,13 @@
+def calc_average(nums):
+  if nums == []:
+    return 0
+  else:
+    number = 0
+    for s in nums:
+      number += s
+    average = number / len(nums)
+    return number
+nums = [1,2,3,4,5,6,7,8,9]
+result = calc_average(nums)
+print(result)
+
