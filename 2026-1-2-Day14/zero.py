40d2b3c8b0461c5366981180225e3f97434bead8
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-01-02 18:28:53 +0900

    Create zero.py

---
+++ 2026-1-2-Day14/zero.py
@@ -0,0 +1,13 @@
+def provess_numbers(x):
+  return x % 3 == 0
+
+nums = [1,2,3,4,5,6,7,8,9]
+result = []
+
+for s in nums:
+  if provess_numbers(s):
+    result.append(0)
+  else:
+    result.append(s)
+
+print(result)
