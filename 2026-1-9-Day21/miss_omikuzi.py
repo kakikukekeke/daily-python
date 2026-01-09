73d0ad331c854c891c92c33445b861bf333322b2
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-01-09 19:47:15 +0900

    Create miss_omikuzi.py

---
+++ 2026-1-9-Day21/miss_omikuzi.py
@@ -0,0 +1,10 @@
+import random
+omikuzi = ["daikiti","tyuukiti","syoukiti","kyou"]
+result = random.choice(omikuzi)
+print(f"Draw a fortune")
+print(f"Yore omikuzi is {result}")
+
+if result == "daikiti" or result == "tyuukiti":
+    print("That's good")
+else:
+    print("That's bad")
