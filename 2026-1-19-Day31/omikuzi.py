6bb842cd0343ffe8d4707201396205a9c3cd94d1
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-01-19 19:12:52 +0900

    Create omikuzi.py

---
+++ 2026-1-19-Day31/omikuzi.py
@@ -0,0 +1,11 @@
+def sayhello2(name):
+    print("hello",name)
+sayhello2("ken")
+
+import random
+def omikuzi():
+    kuzi = ["good","bat"]
+    return random.choice(kuzi)
+
+kekka = omikuzi()
+print(kekka)
