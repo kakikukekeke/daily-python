ba1ac9e27c144c50085b304444f30ec42e98fd29
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-02-12 21:12:20 +0900

    Create math.py

---
+++ 2026-2-12-Day54/math.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("temp.csv",index_col=0,skiprows=1,
+                  encoding="utf-8-sig")
+df2 = pd.read_csv("top_temp.csv",index_col=0,skiprows=1,
+                   encoding="utf-8-sig")
+df3 = pd.read_csv("low_temp.csv",index_col=0,skiprows=1,
+                   encoding="utf-8-sig")
+df.columns = df.columns.str.strip()
+df2.columns = df2.columns.str.strip()
+df3.columns = df3.columns.str.strip()
+
+print(df.columns.values)
+df["年平均気温【℃】"].plot()
+df2["東京都"].plot()
+df3["東京都"].plot()
+plt.show()
+print(df2.columns.values)
+print(df3.columns.values)
