3d962fb74b00fc2831d3b39602cd78d4cfdee8b0
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-02-12 13:54:18 +0900

    Create figure.py

---
+++ 2026-2-12-Day54/figure.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["font.sans-serif"] = ["Hiragino Maru Gothic Pro",
+"Hiragino sans" , "BIZ UDGothic","MS Gothic"]
+
+df = pd.read_csv("population.csv",index_col=
+                 "全国・都道府県",encoding="UTF-8")
+
+df = df.drop("全国",axis=0)
+df["2022年"] = pd.to_numeric(df["2022年"].str.replace(",",""))
+df["2022年"].plot.bar(figsize=(10,6))
+plt.show()
