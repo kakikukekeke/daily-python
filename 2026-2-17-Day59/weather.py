cc5ca72998b3973d852c5c7e9316d655d11f1aac
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-02-17 10:43:42 +0900

    Create weather.py

---
+++ 2026-2-17-Day59/weather.py
@@ -0,0 +1,10 @@
+import json
+from pprint import pprint
+import requests
+
+
+key = "aea2c9e0afcf081dac7e698d18a5435c"
+city = "286-0018,jp"
+url = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={key}&units=metric&lang=ja"
+jsondata = requests.get(url).json()
+pprint(jsondata)
