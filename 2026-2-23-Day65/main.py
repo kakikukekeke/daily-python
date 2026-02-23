06619bd6a8997bcb7fd6e8394b214255c5756fb1
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-02-23 22:44:31 +0900

    Create main.py

---
+++ 2026-2-23-Day65/main.py
@@ -0,0 +1,19 @@
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def introdce(self):
+        print(f"hello I'm{self.name},and {self.age}years old")
+
+    def birthday(self):
+        self.age += 1
+        print(f"{self.name} be {self.age} years old.")
+
+Dog1 = Dog("ken",3)
+Dog2 = Dog("sasuke",5)
+
+Dog1.introdce()
+Dog2.introdce()
+
+Dog1.birthday()
+Dog2.birthday()
