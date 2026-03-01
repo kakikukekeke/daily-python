15628d3cf1506d30c9343ca82bcb06a70998b9e7
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-03-01 23:47:42 +0900

    Create main.py

---
+++ 2026-3-1-Day71/main.py
@@ -0,0 +1,11 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set(font=["Meiryo","Yu Gothic"])
+
+
+df = pd.read_csv("cake_survey.csv")
+plt.plot(df)
+
+plt.show()
