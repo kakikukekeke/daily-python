8a9fc882d5a29516cb27168ff04d838d4aa6ece7
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-03-11 20:23:34 +0900

    Create main.py

---
+++ 2026-3-11-Day81/main.py
@@ -0,0 +1,14 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set(font=["Meiryo","Yu Gothic"])
+
+
+df = pd.read_csv("cake_survey.csv",encoding="utf-8")
+
+sns.boxenplot(data=df,width=0.2)
+plt.show()
+
+
+print(df.median())
