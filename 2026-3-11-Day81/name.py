43a90d380ad819266a1e34d3abd7e6ea45491a9f
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-03-11 20:34:45 +0900

    Create name.py

---
+++ 2026-3-11-Day81/name.py
@@ -0,0 +1,19 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set(font=["Meiryo","Yu Gothic"])
+
+
+df = pd.read_csv("cake_survey.csv",encoding="utf-8")
+
+#sns.boxenplot(data=df,width=0.2)
+df.plot.scatter(x="参加者番号",y="A案",c="b",figsize=(12,8))
+
+x = df.iloc[3]["参加者番号"]
+y = df.iloc[3]["A案"]
+plt.plot(x,y,c="r",marker="X",markersize=15)
+plt.show()
+
+
+print(df.median())
