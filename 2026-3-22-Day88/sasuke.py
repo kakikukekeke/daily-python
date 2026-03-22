e02ac1e1b26659fa65838bdc8d347042b621b4c3
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-03-22 16:48:16 +0900

    Create sasuke.py

---
+++ 2026-3-22-Day88/sasuke.py
@@ -0,0 +1,15 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set(font=["Meiryo","Yu Gothic"])
+
+
+df = pd.read_csv("cake_survey.csv",encoding="utf-8")
+
+df["A案"].plot.hist(bins=10,color="c",ylim=(0.6))
+plt.title("A baratuki")
+plt.show()
+
+df["B案"].plot.hist(bins=10,color="c",ylim=(0.6))
+plt.show()
