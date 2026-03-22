487bf9433cfd39e91f9f40c09b6a6db4961e6550
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-03-22 16:29:02 +0900

    Create main.py

---
+++ 2026-3-22-Day88/main.py
@@ -0,0 +1,16 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set(font=["Meiryo","Yu Gothic"])
+
+
+df = pd.read_csv("cake_survey.csv",encoding="utf-8")
+
+df.plot.scatter(x="参加者番号",y="A案",color="b",ylim=(0,100))
+plt.axhline(y=50, c="Magenta")
+plt.show()
+
+meanA = df["A案"].mean()
+stdA = df["A案"].std()
+print(meanA - stdA ,"~",meanA + stdA)
