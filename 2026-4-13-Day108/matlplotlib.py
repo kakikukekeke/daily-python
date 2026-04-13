32c9f645a10fcae726fc24155b40e2b8206583ba
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-04-13 23:09:58 +0900

    Create matlplotlib.py

---
+++ 2026-4-13-Day108/matlplotlib.py
@@ -0,0 +1,15 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+sns.set(font=["Yu Gothic"])
+
+df = pd.read_csv("sample_data.csv")
+df.head()
+df.plot.scatter(x="order_id",y="discount_rate",c="b")
+plt.show()
+sns.regplot(data=df,x="order_id",y="discount_rate",line_kws={"color":"red"})
+plt.show()
+
+print(df.corr(numeric_only=True)["order_id"]["discount_rate"])
