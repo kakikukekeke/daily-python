30f05dde4666d4c1cc2938c8cedb2b3bff2fb785
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-04-15 20:35:14 +0900

    Create main.py

---
+++ 2026-4-15-Day110/main.py
@@ -0,0 +1,15 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+sns.set(font=["Yu Gothic"])
+
+df = pd.read_csv("sample_data.csv")
+sns.pairplot(data=df)
+plt.show()
+
+
+
+
+print(df.corr(numeric_only=True)["order_id"]["discount_rate"])
