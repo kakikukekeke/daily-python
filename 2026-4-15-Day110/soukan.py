c6a56c22530c47e4780e4491ebd2a08689193604
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-04-15 20:49:03 +0900

    Create soukan.py

---
+++ 2026-4-15-Day110/soukan.py
@@ -0,0 +1,12 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+sns.set()
+
+df = sns.load_dataset("iris")
+df.head()
+df.corr(numeric_only=True)
+sns.pairplot(data=df)
+plt.show()
