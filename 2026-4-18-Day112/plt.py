0768aa1bd1ce8112296ffa22df81b0d05df8740d
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-04-18 22:29:15 +0900

    Create plt.py

---
+++ 2026-4-18-Day112/plt.py
@@ -0,0 +1,15 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+sns.set()
+onespecies = "setosa"
+df = sns.load_dataset("iris")
+df.head()
+one = df[df["species"]==onespecies]
+sns.pairplot(data=one,kind="reg")
+plt.show()
+
+
+df["species"].unique()
