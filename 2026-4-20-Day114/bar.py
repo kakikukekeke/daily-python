5aff2175aadc3b523a1449efafa8c7c61e279185
kakikukekeke <89716163+kakikukekeke@users.noreply.github.com>
2026-04-20 09:15:20 +0900

    Create bar.py

---
+++ 2026-4-20-Day114/bar.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+
+df = pd.read_csv("money.csv",encoding="shift_jis")
+df["お取引日"] = pd.to_datetime(df["お取引日"])
+
+daily_df = df.groupby("お取引日")["お取引金額"].sum().reset_index()
+
+plt.figure(figsize=(10,6))
+sns.barplot(data=daily_df,x="お取引日",y="お取引金額")
+
+
+
+
+print(df.head())
